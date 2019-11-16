bc9694da5421d3d346f1ee4cde5fccd12812189b
GuohongLi <liguohong_edward@163.com>
2019-11-16 16:40:55 +0800

    Update stage_2_meta_embedding.py
    update some attr name

--- config/ImageNet_LT/stage_2_meta_embedding.py
+++ config/ImageNet_LT/stage_2_meta_embedding.py
@@ -17,14 +17,14 @@ training_opt['scheduler_params'] = {'step_size':20, 'gamma':0.1}
 config['training_opt'] = training_opt
 
 networks = {}
-feature_param = {'use_modulatedatt': True, 'use_fc': True, 'dropout': None, 
+feature_param = {'use_selfatt': True, 'use_fc': True, 'dropout': None, 
 			     'stage1_weights': True, 'dataset': training_opt['dataset']}
 feature_optim_param = {'lr': 0.01, 'momentum':0.9, 'weight_decay':0.0005}
 networks['feat_model'] = {'def_file': './models/ResNet10Feature.py',
                           'params': feature_param,
                           'optim_params': feature_optim_param,
                           'fix': False}
-classifier_param = {'in_dim': training_opt['feature_dim'], 'num_classes': training_opt['num_classes'], 
+classifier_param = {'feat_dim': training_opt['feature_dim'], 'num_classes': training_opt['num_classes'], 
 				    'stage1_weights': True, 'dataset': training_opt['dataset']}
 classifier_optim_param = {'lr': 0.1, 'momentum':0.9, 'weight_decay':0.0005}
 networks['classifier'] = {'def_file': './models/MetaEmbeddingClassifier.py',
